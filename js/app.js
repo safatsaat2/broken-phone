2914579092d1479fea812845c4ef68328ea9a92a
safatsaat2 <sakibsafat47@gmail.com>
2023-03-14 23:10:03 +0600

    enetr buttoon fixed

--- js/app.js
+++ js/app.js
@@ -63,10 +63,8 @@ document.getElementById('btn-search').addEventListener('click', function(){
 
 // search input field enter key handler
 document.getElementById('search-field').addEventListener('keypress', function (e) {
-    if (e.key === 'enter') {
+    if (e.key === 'Enter') {
         processSearch(10);
-    }{
-        
     }
 });
 
